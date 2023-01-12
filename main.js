40835f48ad00c18bb57e559da0bea324c0e4d653
kuttijoe <kuttidme1113@gmail.com>
2023-01-12 11:58:12 +0530

    adding

---
+++ main.js
@@ -0,0 +1,19 @@
+$('.brand').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+		320:{
+            items:1
+        },
+        700:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
